ca4d297dae8b6287bc0df2d97f2a3180292c28ad
visborne <visborne@proton.me>
2022-07-15 18:05:26 +0200

    rudimentary animation support (but no animation)

--- src/webgl_clojurescript_tutorial/core.cljs
+++ src/webgl_clojurescript_tutorial/core.cljs
@@ -7,13 +7,14 @@
       [thi.ng.geom.gl.glmesh :as glmesh]
       [thi.ng.geom.gl.camera :as cam]
       [thi.ng.geom.gl.shaders :as shaders]
-      [thi.ng.geom.gl.webgl.constants :as glc]))
+      [thi.ng.geom.gl.webgl.constants :as glc]
+      [thi.ng.geom.gl.webgl.animator :as anim] ))
 
 (enable-console-print!)
 
 (defonce gl-ctx (gl/gl-context "main"))
 
-(defonce camera (cam/perspective-camera {}))
+(defonce camera (cam/perspective-camera {:aspect 1.0}))
 
 (println camera)
 
@@ -39,10 +40,19 @@
     (gl/make-buffers-in-spec gl-ctx glc/static-draw)
     (cam/apply camera)))
 
-(doto gl-ctx
-  (gl/clear-color-and-depth-buffer 0 0 0 1 1)
-  (gl/draw-with-shader (combine-model-shader-and-camera triangle shader-spec camera)))
+(defn spin
+  [t]
+  (geom/rotate-y mat/M44 (/ t 10)))
 
+(defn draw-frame! []
+  (doto gl-ctx
+        (gl/clear-color-and-depth-buffer 0 0 0 1 1)
+        (gl/draw-with-shader (combine-model-shader-and-camera triangle shader-spec camera))))
+
+(println (combine-model-shader-and-camera triangle shader-spec camera))
+
+(defonce running
+  (anim/animate (fn [t] (draw-frame!) true)))
 
 
 (println "Hello, World!")
